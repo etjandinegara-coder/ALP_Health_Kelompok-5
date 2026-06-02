98ad1a672a5118a91f17f657f4243e7cf190cdf4
etjandinegara-coder <etjandinegara@student.ciputra.ac.id>
2026-06-02 15:49:55 +0800

    another commit

--- src/main/java/alp/semester2/tugas/model/User.java
+++ src/main/java/alp/semester2/tugas/model/User.java
@@ -1,10 +1,18 @@
 package alp.semester2.tugas.model;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
